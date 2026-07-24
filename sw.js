d19c7ec9ddffdb0bb76fc006901a98499e1cab2a
zhafranrfi <zhafranrfi@users.noreply.github.com>
2026-07-25 01:04:03 +0700

    Update pair, close price, dan layout mobile

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'trading-journal-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
